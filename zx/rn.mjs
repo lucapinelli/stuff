8df28908b5e8fdd03f9e07f0a3a84bb933190cdd
Luca Pinelli <pinelli.luca@gmail.com>
2021-10-20 07:27:01 +0200

    zx/rn.mjs

--- zx/rn.mjs
+++ zx/rn.mjs
@@ -1,11 +1,55 @@
 #!/usr/bin/env zx
 // zx version 4.2.0
 
-/* global argv $ */
+/* global $ */
+
+const minimist = require('minimist')
+
+const printInfo = () => {
+  console.info('rn version 0.2.0')
+  process.exit(0)
+}
+
+const printHelp = () => {
+  console.info(`
+    NAME
+      rn.mjs - rename files and directories
+
+    SYNOPSIS
+      rn.mjs SEARCH REPLACE
+
+    OPTIONS
+      --simulate  print names of files to be renamed, but don't rename.
+      --fd        argument to pass to the \`fd\` command
+      --debug     print debug info
+      --verbose   prints all executed commands alongside with their outputs
+      --help      display this help and exit
+      --info      output version information and exit
+    `)
+  process.exit(0)
+}
+
+const args = minimist(
+  process.argv.slice(3),
+  {
+    boolean: ['simulate', 'debug', 'verbose', 'help', 'info'],
+    string: ['fd']
+  }
+)
+const [search, replace, unknown] = args._
+const { simulate, fd, debug, verbose, help, info } = args
+$.verbose = verbose
+
+if (help || unknown) {
+  printHelp()
+}
+if (info) {
+  printInfo()
+}
 
 const log = {
   debug: (...args) => {
-    if (argv.debug === true) {
+    if (debug === true) {
       console.debug(...args)
     }
   },
@@ -13,42 +57,15 @@ const log = {
   error: console.error
 }
 
-if (argv.info) {
-  log.info('rn version 0.1.0')
-  process.exit(0)
-}
-
-if (argv.help || argv.h || argv._.length !== 3) {
-  log.info(`
-  NAME
-    rn.mjs - rename files ad directory
-
-  SYNOPSIS
-    rn.mjs SEARCH REPLACE
-
-  OPTIONS
-    --simulate  print names of files to be renamed, but don't rename.
-    --fd        argument to pass to the \`fd\` command
-    --debug     print debug info
-    --verbose   prints all executed commands alongside with their outputs
-    --help      display this help and exit
-    --info      output version information and exit
-  `)
-  process.exit(0)
-}
-
-// eslint-disable-next-line no-unused-vars
-const [_, search, replace] = argv._
-
-$.verbose = argv.verbose === true
-
-log.debug('🤖 argv', argv)
+log.debug('🤖 args %j', {
+  simulate, fd, debug, verbose, help, info, search, replace
+})
 
 // the main is implemented only to remove the eslint fatal error "Cannot use keyword 'await' outside an async function"
 const main = async () => {
   try {
-    const list = (await $`fd ${argv.fd}`).stdout.split('\n').filter(Boolean)
-    if (!list.length && argv.fd) {
+    const list = (await $`fd ${fd || ''}`).stdout.split('\n').filter(Boolean)
+    if (!list.length && fd) {
       log.info('🤖 no files matches the fd filter.')
     }
     let file = null; let rename = null
@@ -57,8 +74,8 @@ const main = async () => {
       rename = file.replace(new RegExp(search), replace)
       if (file !== rename) {
         log.info('🤖 file       %s', file)
-        log.info('   renamed as %s\n', rename)
-        if (!argv.simulate) {
+        log.info('   renamed as %s', rename)
+        if (!simulate) {
           await $`mv ${file} ${rename}`
         }
       }
