e4b0cfb035016730e1a4f674a1f481366bac1f3c
Luca Pinelli <pinelli.luca@gmail.com>
2024-06-22 17:28:57 +0200

    sh/suunto_report

--- sql/freedive.sql
+++ sql/freedive.sql
@@ -3,11 +3,14 @@
 -- " | sqlite3 DM420240324.db
 
 -- FreeDive VIEW
--- drop view FreeDive
+-- drop view FreeDive;
 create view FreeDive
 as
 select
   DiveId,
+  cast(strftime('%Y', date(datetime(((StartTime / 10000000) - (1970 * 365 * 24 * 60 * 60) - (112 * 24 * 60 * 60)), 'unixepoch'))) as int) Year,
+  cast(strftime('%m', date(datetime(((StartTime / 10000000) - (1970 * 365 * 24 * 60 * 60) - (112 * 24 * 60 * 60)), 'unixepoch'))) as int) Month,
+  cast(strftime('%d', date(datetime(((StartTime / 10000000) - (1970 * 365 * 24 * 60 * 60) - (112 * 24 * 60 * 60)), 'unixepoch'))) as int) Day,
   datetime(((StartTime / 10000000) - (1970 * 365 * 24 * 60 * 60) - (112 * 24 * 60 * 60)), 'unixepoch') DiveDate,
   Duration,
   (Duration / 60) || ':' || substr('0' || (Duration % 60), -2, 2) as ApneaTime,
@@ -17,6 +20,30 @@ select
   DiveNumberInSerie as Serie
 from dive
 where mode = 3 --freedive
+
+-- compare 12 month ago Duration
+select DiveDate, Duration, ApneaTime, AvgDepth, MaxDepth, Temperature, Serie
+from freedive
+where (Year <= 2023 and Month <= 06)
+  or (Year = 2024 and Month = 06)
+order by Duration desc, DiveDate
+limit 10
+
+-- compare 12 month ago MaxDepth
+select DiveDate, Duration, ApneaTime, AvgDepth, MaxDepth, Temperature, Serie
+from freedive
+where (Year <= 2023 and Month <= 06)
+  or (Year = 2024 and Month = 06)
+order by MaxDepth desc, DiveDate
+limit 10
+
+-- compare 12 month ago AvgDepth
+select DiveDate, Duration, ApneaTime, AvgDepth, MaxDepth, Temperature, Serie
+from freedive
+where (Year <= 2023 and Month <= 06)
+  or (Year = 2024 and Month = 06)
+order by AvgDepth desc, DiveDate
+limit 10
 
 -- Monthly Temperature
 select *
@@ -65,3 +92,24 @@ select DiveDate, ApneaTime, AvgDepth, MaxDepth, Temperature, Serie
 from FreeDive
 order by AvgDepth desc, DiveDate
 limit 10
+
+-- ApneaTime
+select Year, Month, max(ApneaTime) ApneaTime
+from FreeDive
+group by Year, Month
+--order by Year, Month
+order by max(ApneaTime) desc
+
+-- MaxDepth
+select Year, Month, max(MaxDepth) MaxDepth
+from FreeDive
+group by Year, Month
+--order by Year, Month
+order by max(MaxDepth) desc
+
+-- AvgDepth
+select Year, Month, max(AvgDepth) AvgDepth
+from FreeDive
+group by Year, Month
+--order by Year, Month
+order by max(AvgDepth) desc
